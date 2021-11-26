60515f58c7c382c3f6d3ee595a3646ca14b94a9f
Airidas Kutra <airidas.kutra@mif.stud.vu.lt>
2021-11-26 19:57:52 +0200

    Added missing functions

--- Video player test 2/IPlaybackController.h
+++ Video player test 2/IPlaybackController.h
@@ -13,8 +13,8 @@ public:
 
     virtual void Play() = 0;
     virtual void Pause() = 0;
-    virtual void SetVolume(float volume) = 0;
-    virtual void SetBalance(float balance) = 0;
+    virtual void SetVolume(float volume, bool bounded = true) = 0;
+    virtual void SetBalance(float balance, bool bounded = true) = 0;
     virtual void Seek(TimePoint time) = 0;
     virtual void SetVideoStream(int index) = 0;
     virtual void SetAudioStream(int index) = 0;
@@ -24,11 +24,12 @@ public:
     virtual float GetVolume() const = 0;
     virtual float GetBalance() const = 0;
     virtual TimePoint CurrentTime() const = 0;
+    virtual Duration GetDuration() const = 0;
+    virtual Duration GetBufferedDuration() const = 0;
+
     virtual int CurrentVideoStream() const = 0;
     virtual int CurrentAudioStream() const = 0;
     virtual int CurrentSubtitleStream() const = 0;
-    virtual Duration GetDuration() const = 0;
-
     virtual std::vector<std::string> GetAvailableVideoStreams() const = 0;
     virtual std::vector<std::string> GetAvailableAudioStreams() const = 0;
     virtual std::vector<std::string> GetAvailableSubtitleStreams() const = 0;
