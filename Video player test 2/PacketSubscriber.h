d4352ede34c7762cadae645622d37ae0bef19000
Airidas Kutra <airidas.kutra@mif.stud.vu.lt>
2022-01-14 01:34:35 +0200

    Added utility class for basic packet receiving

--- Video player test 2/PacketSubscriber.h
+++ Video player test 2/PacketSubscriber.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "NetBase2.h"
+#include "PacketTypes.h"
 
 namespace znet
 {
@@ -14,5 +15,31 @@ namespace znet
         PacketSubscriber(int32_t packetId);
         PacketSubscriber(const PacketSubscriber&) = delete;
         virtual ~PacketSubscriber();
+    };
+
+    class PacketReceiver : public PacketSubscriber
+    {
+        std::queue<std::pair<Packet, int64_t>> _packets;
+        std::mutex _m_packets;
+
+        void _OnPacketReceived(Packet packet, int64_t userId)
+        {
+            std::lock_guard<std::mutex> lock(_m_packets);
+            _packets.push({ std::move(packet), userId });
+        }
+    public:
+        PacketReceiver(PacketType packetType) : PacketSubscriber((int32_t)packetType) {}
+        size_t PacketCount()
+        {
+            std::lock_guard<std::mutex> lock(_m_packets);
+            return _packets.size();
+        }
+        std::pair<Packet, int64_t> GetPacket()
+        {
+            std::lock_guard<std::mutex> lock(_m_packets);
+            std::pair<Packet, int64_t> packet = std::move(_packets.front());
+            _packets.pop();
+            return packet;
+        }
     };
 }
