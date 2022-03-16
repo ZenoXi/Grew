c0ef635a5c6a4fe476940013372caa5afffdb51f
Airidas Kutra <airidas.kutra@mif.stud.vu.lt>
2022-03-16 21:51:05 +0200

    Changed `Loading()` to also return a message

--- Video player test 2/IPlaybackController.h
+++ Video player test 2/IPlaybackController.h
@@ -5,10 +5,15 @@
 #include <vector>
 #include <string>
 
-
 class IPlaybackController
 {
 public:
+    struct LoadingInfo
+    {
+        bool loading;
+        std::wstring message;
+    };
+
     virtual void Update() = 0;
 
     virtual void Play() = 0;
@@ -20,9 +25,9 @@ public:
     virtual void SetAudioStream(int index) = 0;
     virtual void SetSubtitleStream(int index) = 0;
 
-    virtual bool Paused() const = 0;
-    virtual bool Loading() const = 0;
-    virtual bool Finished() const = 0;
+    virtual bool Paused() const = 0; // The play button is in the 'paused' state. !Paused() does not always mean that the playback is running
+    virtual LoadingInfo Loading() const = 0; // The playback cannot start because something needs to be done
+    virtual bool Finished() const = 0; // The playback has reached the end
     virtual float GetVolume() const = 0;
     virtual float GetBalance() const = 0;
     virtual TimePoint CurrentTime() const = 0;
