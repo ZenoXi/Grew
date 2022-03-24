0be85fdd6af9eb923a1915e48db8287b3ca7da06
Airidas Kutra <airidas.kutra@mif.stud.vu.lt>
2022-03-25 01:12:47 +0200

    Included more libraries

--- Video player test 2/Graphics.h
+++ Video player test 2/Graphics.h
@@ -2,9 +2,12 @@
 
 #pragma comment( lib,"d2d1.lib" )
 #pragma comment( lib,"d3d11.lib" )
+#pragma comment( lib,"dxguid.lib" )
 #pragma comment( lib,"dwrite.lib" )
 #include <d2d1_1.h>
 #include <d3d11_1.h>
+#include <d2d1effects.h>
+#include <d2d1effects_2.h>
 #include "Dwrite.h"
 
 struct Graphics
