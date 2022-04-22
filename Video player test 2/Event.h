a8f00bb95fc9baf41b54d391aa7ac28f7d941521
Airidas Kutra <airidas.kutra@mif.stud.vu.lt>
2022-04-22 20:22:43 +0300

    Added event identifiers and event removal

--- Video player test 2/Event.h
+++ Video player test 2/Event.h
@@ -3,20 +3,43 @@
 #include <vector>
 #include <functional>
 
+struct EventInfo
+{
+    void* ownerPtr;
+    std::string name;
+};
+
 template<class _Ret, class... _Types>
 class Event
 {
     std::vector<std::function<_Ret(_Types...)>> _handlers;
+    std::vector<EventInfo> _info;
     typedef typename std::vector<std::function<_Ret(_Types...)>>::iterator iterator;
     typedef typename std::vector<std::function<_Ret(_Types...)>>::const_iterator const_iterator;
 
 public:
     Event() {}
 
-    void Add(const std::function<_Ret(_Types...)>& handler)
+    void Add(const std::function<_Ret(_Types...)>& handler, EventInfo info = { nullptr, "" })
     {
         _handlers.push_back(handler);
-        //handler();
+        _info.push_back(info);
+    }
+
+    void Remove(EventInfo info)
+    {
+        if (info.ownerPtr == nullptr && info.name == "")
+            return;
+
+        for (int i = 0; i < _info.size(); i++)
+        {
+            if (_info[i].ownerPtr == info.ownerPtr && _info[i].name == info.name)
+            {
+                _handlers.erase(_handlers.begin() + i);
+                _info.erase(_info.begin() + i);
+                break;
+            }
+        }
     }
 
     void InvokeAll(_Types... args)
