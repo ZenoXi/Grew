3be7c5f01fab1c0a97cb98b38e723e268eb08933
Airidas Kutra <airidas.kutra@mif.stud.vu.lt>
2022-07-13 16:32:03 +0300

    Added a basic audiogram component

---
+++ Video player test 2/Waveform.cpp
@@ -0,0 +1,68 @@
+#include "Waveform.h"
+#include "App.h"
+
+void zcom::Waveform::_OnUpdate()
+{
+    if (ztime::Main() - _lastUpdate < _updateInterval)
+        return;
+    _lastUpdate = ztime::Main();
+
+    auto adapter = _scene->GetApp()->playback.AudioAdapter();
+    if (adapter)
+    {
+        auto data = adapter->GetRecentSampleData();
+
+        for (int frange = 1; frange < 500; frange++)
+        {
+            float lAmpComb = 0.0f;
+            float rAmpComb = 0.0f;
+            //Pos2D<float> lVecComb = 0.0f;
+            //Pos2D<float> rVecComb = 0.0f;
+
+            for (int f = frange * 10; f < frange * 10 + 1; f++)
+            {
+                float lAmp = 0.0f;
+                float rAmp = 0.0f;
+                //Pos2D<float> lVec = 0.0f;
+                //Pos2D<float> rVec = 0.0f;
+                for (size_t sample = 0; sample < data.size(); sample++)
+                {
+                    float t = sample / (float)data[sample].sampleRate;
+                    auto point = point_rotated_by({ 0.0f, 0.0f }, { 1.0f, 0.0f }, 2 * 3.1415f * t * f);
+                    lAmp += (point * (data[sample].data[0] / 32767.0f)).x;
+                    rAmp += (point * (data[sample].data[1] / 32767.0f)).x;
+                    //lVec += point * (data[sample].data[0] / 32767.0f);
+                    //rVec += point * (data[sample].data[1] / 32767.0f);
+                    //lPoints[sample - tail] = point * (lData[sample] / 32767.0f);
+                    //rPoints[sample - tail] = point * (rData[sample] / 32767.0f);
+                }
+
+                lAmp /= data.size();
+                rAmp /= data.size();
+                //lVec = lVec / float(data.size());
+                //rVec = rVec / float(data.size());
+
+                lAmpComb += lAmp;
+                rAmpComb += rAmp;
+                //lVecComb += lVec;
+                //rVecComb += rVec;
+            }
+
+            lAmpComb /= 1.0f;
+            rAmpComb /= 1.0f;
+            //lVecComb = lVecComb / 1.0f;
+            //rVecComb = rVecComb / 1.0f;
+
+            _data.lAmps[frange - 1] = lAmpComb;
+            _data.rAmps[frange - 1] = rAmpComb;
+        }
+    }
+    else
+    {
+        for (int i = 0; i < 500; i++)
+        {
+            _data.lAmps[i] = 0;
+            _data.rAmps[i] = 0;
+        }
+    }
+}
---
+++ Video player test 2/Waveform.h
@@ -0,0 +1,91 @@
+#pragma once
+
+#include "ComponentBase.h"
+#include "Functions.h"
+#include "Transition.h"
+
+#include <fstream>
+#include <Windows.h>
+
+#pragma comment(lib, "Winmm.lib")
+#include <mmsystem.h>
+
+namespace zcom
+{
+    // Completely barebones component, only contains base component functionality
+    class Waveform : public Base
+    {
+#pragma region base_class
+    protected:
+        void _OnUpdate();
+        bool _Redraw() { return GetVisible(); }
+        void _OnDraw(Graphics g)
+        {
+            ID2D1SolidColorBrush* brush;
+            g.target->CreateSolidColorBrush(D2D1::ColorF(0.8f, 0.8f, 0.8f), &brush);
+
+            for (int i = 0; i < 498; i++)
+            {
+                _transitions[i].Apply(_values[i]);
+                if (_data.lAmps[i] > _values[i])
+                {
+                    _values[i] = _data.lAmps[i];
+                    _transitions[i].Start(_values[i], 0.0f);
+                }
+
+                D2D1_RECT_F rect;
+                rect.left = 2 * i + 0.0f;
+                rect.right = 2 * i + 2.0f;
+                rect.top = 0.0f;
+                rect.bottom = _values[i] * 1000.0f;
+                g.target->FillRectangle(rect, brush);
+            }
+
+            brush->Release();
+        }
+        void _OnResize(int width, int height) {}
+
+    public:
+        const char* GetName() const { return "waveform"; }
+#pragma endregion
+
+    private:
+        struct WaveData
+        {
+            std::unique_ptr<float[]> lAmps;
+            std::unique_ptr<float[]> rAmps;
+            TimePoint time;
+
+            WaveData()
+            {
+                lAmps = std::make_unique<float[]>(500);
+                rAmps = std::make_unique<float[]>(500);
+            }
+        };
+
+        WaveData _data;
+        std::vector<float> _values;
+        std::vector<Transition<float>> _transitions;
+        Duration _updateInterval = Duration(25, MILLISECONDS);
+        TimePoint _lastUpdate = ztime::Main();
+
+    protected:
+        friend class Scene;
+        friend class Base;
+        Waveform(Scene* scene) : Base(scene) {}
+        void Init()
+        {
+            for (int i = 0; i < 498; i++)
+            {
+                _values.push_back(0.0f);
+                _transitions.push_back(Transition<float>(Duration(200, MILLISECONDS)));
+            }
+        }
+    public:
+        ~Waveform() {}
+        Waveform(Waveform&&) = delete;
+        Waveform& operator=(Waveform&&) = delete;
+        Waveform(const Waveform&) = delete;
+        Waveform& operator=(const Waveform&) = delete;
+    };
+}
