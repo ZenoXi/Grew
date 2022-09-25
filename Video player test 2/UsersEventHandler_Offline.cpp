c7fc0a7b30abc17cd84964c790bfaef02a12a3ca
Airidas Kutra <airidas.kutra@mif.stud.vu.lt>
2022-09-25 23:34:57 +0300

    Offline user handler now has a local user with all permissions

--- Video player test 2/UsersEventHandler_Offline.cpp
+++ Video player test 2/UsersEventHandler_Offline.cpp
@@ -4,4 +4,17 @@
 UsersEventHandler_Offline::UsersEventHandler_Offline(Users_Internal* users) : IUsersEventHandler(users)
 {
     _users->users.clear();
+
+    // Add the local user with all permissions
+    auto localUser = std::make_unique<User>();
+    localUser->id = -1;
+    localUser->AllowAll(true);
+    _users->thisUserId = -1;
+    _users->users.push_back(std::move(localUser));
+}
+
+UsersEventHandler_Offline::~UsersEventHandler_Offline()
+{
+    // Clear the local user
+    _users->users.clear();
 }
--- Video player test 2/UsersEventHandler_Offline.h
+++ Video player test 2/UsersEventHandler_Offline.h
@@ -12,4 +12,5 @@ public:
     void Update() {}
 
     UsersEventHandler_Offline(Users_Internal* users);
+    ~UsersEventHandler_Offline();
 };
